2d1502890470935c4cd453c029c2777e6bb57258
EC2 Default User <baranasoftware@gmail.com>
2026-06-27 22:05:00 +0000

    add second example

--- transcribe.py
+++ transcribe.py
@@ -6,8 +6,5 @@ for file in os.listdir("data"):
     if file.endswith((".mp4", ".mkv", ".mov", ".mp3", ".wav")):
         path = os.path.join("data", file)
         result = model.transcribe(path)
-        mel = whisper.log_mel_spectrogram(result, n_mels=model.dims.n_mels).to(model.device)
-        _, probs = model.detect_language(mel)
-        print(f"Detected language: {max(probs, key=probs.get)}")
         with open(path + ".srt", "w") as f:
             f.write(result["text"])
