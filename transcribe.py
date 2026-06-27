c3506e6c6b6dc5ccfce506f67543b0174388bef8
EC2 Default User <baranasoftware@gmail.com>
2026-06-27 21:49:22 +0000

    add whsiper example

--- transcribe.py
+++ transcribe.py
@@ -1,5 +1,13 @@
 import whisper
+import os
 
 model = whisper.load_model("turbo")
-result = model.transcribe("data/SafeCerebralProtectionDoublePatchTechniqueforAorticandMitralValveReplacement_1080p.mp4")
-print(result["text"])
+for file in os.listdir("data"):
+    if file.endswith((".mp4", ".mkv", ".mov", ".mp3", ".wav")):
+        path = os.path.join("data", file)
+        result = model.transcribe(path)
+        mel = whisper.log_mel_spectrogram(result, n_mels=model.dims.n_mels).to(model.device)
+        _, probs = model.detect_language(mel)
+        print(f"Detected language: {max(probs, key=probs.get)}")
+        with open(path + ".srt", "w") as f:
+            f.write(result["text"])
